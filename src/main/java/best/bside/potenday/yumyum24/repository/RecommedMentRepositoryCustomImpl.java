99d1b2a09767d05e7f12718684a6845805b87163
cmpark <opopqkr1@gmail.com>
2024-02-04 10:01:15 +0900

    추천 멘트 수정

--- src/main/java/best/bside/potenday/yumyum24/repository/RecommedMentRepositoryCustomImpl.java
+++ src/main/java/best/bside/potenday/yumyum24/repository/RecommedMentRepositoryCustomImpl.java
@@ -3,6 +3,7 @@ package best.bside.potenday.yumyum24.repository;
 import best.bside.potenday.yumyum24.domain.QRecommendMent;
 
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class RecommedMentRepositoryCustomImpl extends BaseRepository implements RecommedMentRepositoryCustom {
 
@@ -12,7 +13,8 @@ public class RecommedMentRepositoryCustomImpl extends BaseRepository implements 
 
         return select(m.ment)
                 .from(m)
-                .where(m.startTime.after(now).and(m.endTime.before(now)))
+                .where(m.startTime.goe(LocalTime.parse(DateTimeFormatter.ofPattern("HH:mm").format(now)))
+                        .and(m.endTime.loe(LocalTime.parse(DateTimeFormatter.ofPattern("HH:mm").format(now)))))
                 .fetchOne();
     }
 }
